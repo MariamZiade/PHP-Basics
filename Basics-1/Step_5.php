fbabcdf433e7ba1cc3f618546a9321e0041ce635
Mariam Ziade <mariam.ziade@gmail.com>
2021-05-24 15:44:31 +0300

    step 5

---
+++ Basics-1/Step_5.php
@@ -0,0 +1,12 @@
+<?php 
+
+$course_name = "Wordpress";
+$enrolled_students = 30;
+$price = 29.99;
+$on_discount = "Yes / No";
+
+echo "Course title:$course_name <br>
+Enrolled Students: $enrolled_students <br>
+Course price: $price $ USD <br>
+Course on discount: $on_discount"
+?>
