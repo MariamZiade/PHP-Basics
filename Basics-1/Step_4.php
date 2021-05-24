1f296c93435cd8ffb43954d1f5a7d92e6a7eceb7
Mariam Ziade <mariam.ziade@gmail.com>
2021-05-24 15:29:44 +0300

    Step 4

--- Basics-1/Step_4.php
+++ Basics-1/Step_4.php
@@ -1,4 +1,9 @@
 <?php
 
+$name = "mariam";
+$position = "UI UX Designer";
+$github = "https://github.com/mariamziade";
+
+echo "Hello, I'm $name, I'm a $position please check my github link $github";
 
 ?>
