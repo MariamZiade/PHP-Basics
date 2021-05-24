08cc70d611eacc199561a90924600efd4820870d
Mariam Ziade <mariam.ziade@gmail.com>
2021-05-24 12:41:11 +0300

    Step 3

---
+++ Step_3.php
@@ -0,0 +1,12 @@
+<?php
+$name = "mariam";
+$email = "mariam.ziade@gmail.com";
+
+echo "My name is $name and my email is $email <br>";
+
+$height = 100;
+$width = 50;
+$area = "$height" * "$width";
+echo "$area";
+
+?>
